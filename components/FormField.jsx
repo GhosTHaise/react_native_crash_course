9eb74d18d4817f45cf1bcba42499e2a2d5084572
Fitiavana Sambatra <Ghostrex2@gmail.com>
2024-05-09 23:05:40 +0300

    feat : custom form field

--- components/FormField.jsx
+++ components/FormField.jsx
@@ -1,18 +1,31 @@
-import { View, Text } from 'react-native'
+import { View, Text, TextInput } from 'react-native'
 import React from 'react'
 
 const FormField = ({
     title,
+    placeholder,
     value,
     handleChangeText,
     otherStyles,
     keyboardType
 }) => {
     return (
-        <View className='space-y-2'>
+        <View className={`space-y-2 ${otherStyles}`}>
             <Text className='text-base text-gray-100 font-pmedium'>
-                FormField
+                {title}
             </Text>
+
+            <View className='border-2 border-black-200 w-full h-16 px-4
+             bg-black-100 rounded-2xl focus:border-secondary items-center'>
+                <TextInput
+                    className='flex-1 tetx-white font-psemibold text-base'
+                    value={value}
+                    placeholder={placeholder}
+                    placeholderTextColor="#7b7b8b"
+                    onChangeText={handleChangeText}
+                    secureTextEntry
+                />
+            </View>
         </View>
     )
 }
