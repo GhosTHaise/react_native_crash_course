696687ee87b66e293798a97d36cc2b8b3232bf26
Fitiavana Sambatra <Ghostrex2@gmail.com>
2024-05-10 19:48:47 +0300

    feat : show or hidden password

--- components/FormField.jsx
+++ components/FormField.jsx
@@ -1,6 +1,6 @@
-import { View, Text, TextInput } from 'react-native'
-import React from 'react'
-
+import { View, Text, TextInput, TouchableOpacity, Image } from 'react-native'
+import React, { useState } from 'react'
+import { icons } from '../constants'
 const FormField = ({
     title,
     placeholder,
@@ -9,6 +9,8 @@ const FormField = ({
     otherStyles,
     keyboardType
 }) => {
+    const [showPassword, setShowPassword] = useState(false)
+
     return (
         <View className={`space-y-2 ${otherStyles}`}>
             <Text className='text-base text-gray-100 font-pmedium'>
@@ -16,15 +18,27 @@ const FormField = ({
             </Text>
 
             <View className='border-2 border-black-200 w-full h-16 px-4
-             bg-black-100 rounded-2xl focus:border-secondary items-center'>
+             bg-black-100 rounded-2xl focus:border-secondary items-center flex-row'>
                 <TextInput
-                    className='flex-1 tetx-white font-psemibold text-base'
+                    className='flex-1 text-white font-psemibold text-base'
                     value={value}
                     placeholder={placeholder}
                     placeholderTextColor="#7b7b8b"
                     onChangeText={handleChangeText}
-                    secureTextEntry
+                    secureTextEntry={title === "Password" && !showPassword}
                 />
+
+                {
+                    title === "Password" && (
+                        <TouchableOpacity onPress={() => setShowPassword(!showPassword)}>
+                            <Image
+                                className='w-6 h-6'
+                                resizeMode='contain'
+                                source={!showPassword ? icons.eye : icons.eyeHide}
+                            />
+                        </TouchableOpacity>
+                    )
+                }
             </View>
         </View>
     )
