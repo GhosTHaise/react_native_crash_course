40c0254d0e0ec7b3cc772cf844c8e3c821ecb262
Fitiavana Sambatra <Ghostrex2@gmail.com>
2024-05-25 19:54:49 +0300

    feat : search page

--- app/search/[query].jsx
+++ app/search/[query].jsx
@@ -1,13 +1,79 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import { Alert, FlatList, Image, RefreshControl, Text, View } from 'react-native'
+import React, { useEffect, useState } from 'react'
+import { SafeAreaView } from 'react-native-safe-area-context'
+import { images } from '../../constants'
+import SearchInput from '../../components/SearchInput'
+import Trending from '../../components/Trending'
+import EmptyState from '../../components/EmptyState'
+import { getAllPosts, getLatestPosts, searchPosts } from '../../lib/appwrite'
+import useAppwrite from '../../lib/useAppwrite'
+import VideoCard from '../../components/VideoCard'
 import { useLocalSearchParams } from 'expo-router'
-import { SafeAreaView } from 'react-native-safe-area-context';
 
 const Search = () => {
-    const { query } = useLocalSearchParams();
+    const { query } = useLocalSearchParams()
+
+    const { data: posts, refetch, isLoading } = useAppwrite(searchPosts(query));
+
+    const onRefresh = async () => {
+        setRefreshing(true);
+        //re call videos -> if any new videos appeared
+        await refetch();
+        setRefreshing(false);
+    }
+
+    useEffect(() => {
+        refetch()
+    }, [query])
+
     return (
-        <SafeAreaView className='bg-primary h-full'>
-            <Text className='text-3xl text-white'>{query}</Text>
+        <SafeAreaView
+            className='bg-primary h-full'
+        >
+            <FlatList
+                data={posts}
+                keyExtractor={(item) => item.$id}
+                renderItem={({ item }) => (
+                    <VideoCard
+                        video={item}
+                    />
+                )}
+                ListHeaderComponent={() => (
+                    <View className="my-6 px-4 space-y-6">
+                        <View className="justify-between items-start flex-row mb-6">
+                            <View>
+                                <Text className='font-pmedium text-sm text-gray-100'>
+                                    Search Results
+                                </Text>
+                                <Text className='text-2xl font-psemibold text-white'>
+                                    {query}
+                                </Text>
+                            </View>
+                            <View className='mt-1.5'>
+                                <Image
+                                    source={images.logoSmall}
+                                    className='w-9 h-10'
+                                    resizeMethod='contain'
+                                />
+                            </View>
+                        </View>
+
+                        <SearchInput />
+
+                        <View className='w-full flex-1 pt-5 pb-8'>
+                            <Text className='text-gray-100 text-lg font-pregular mb-3'>
+                                Latest Videos
+                            </Text>
+                        </View>
+                    </View>
+                )}
+                ListEmptyComponent={() => (
+                    <EmptyState
+                        title="No Videos Found"
+                        subtitle="Be the first one to upload a video ."
+                    />
+                )}
+            />
         </SafeAreaView>
     )
 }
