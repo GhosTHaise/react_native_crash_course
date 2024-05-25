9cb13889af49e861e2c3b8719a129b6071afb4f3
Fitiavana Sambatra <Ghostrex2@gmail.com>
2024-05-25 21:40:44 +0300

    feat : search

--- app/search/[query].jsx
+++ app/search/[query].jsx
@@ -1,11 +1,9 @@
-import { Alert, FlatList, Image, RefreshControl, Text, View } from 'react-native'
-import React, { useEffect, useState } from 'react'
+import { FlatList, Text, View } from 'react-native'
+import React, { useEffect } from 'react'
 import { SafeAreaView } from 'react-native-safe-area-context'
-import { images } from '../../constants'
 import SearchInput from '../../components/SearchInput'
-import Trending from '../../components/Trending'
 import EmptyState from '../../components/EmptyState'
-import { getAllPosts, getLatestPosts, searchPosts } from '../../lib/appwrite'
+import { searchPosts } from '../../lib/appwrite'
 import useAppwrite from '../../lib/useAppwrite'
 import VideoCard from '../../components/VideoCard'
 import { useLocalSearchParams } from 'expo-router'
@@ -13,14 +11,7 @@ import { useLocalSearchParams } from 'expo-router'
 const Search = () => {
     const { query } = useLocalSearchParams()
 
-    const { data: posts, refetch, isLoading } = useAppwrite(searchPosts(query));
-
-    const onRefresh = async () => {
-        setRefreshing(true);
-        //re call videos -> if any new videos appeared
-        await refetch();
-        setRefreshing(false);
-    }
+    const { data: posts, refetch } = useAppwrite(() => searchPosts(query));
 
     useEffect(() => {
         refetch()
@@ -39,38 +30,22 @@ const Search = () => {
                     />
                 )}
                 ListHeaderComponent={() => (
-                    <View className="my-6 px-4 space-y-6">
-                        <View className="justify-between items-start flex-row mb-6">
-                            <View>
-                                <Text className='font-pmedium text-sm text-gray-100'>
-                                    Search Results
-                                </Text>
-                                <Text className='text-2xl font-psemibold text-white'>
-                                    {query}
-                                </Text>
-                            </View>
-                            <View className='mt-1.5'>
-                                <Image
-                                    source={images.logoSmall}
-                                    className='w-9 h-10'
-                                    resizeMethod='contain'
-                                />
-                            </View>
-                        </View>
-
-                        <SearchInput />
-
-                        <View className='w-full flex-1 pt-5 pb-8'>
-                            <Text className='text-gray-100 text-lg font-pregular mb-3'>
-                                Latest Videos
-                            </Text>
+                    <View className="my-6 px-4">
+                        <Text className='font-pmedium text-sm text-gray-100'>
+                            Search Results
+                        </Text>
+                        <Text className='text-2xl font-psemibold text-white'>
+                            {query}
+                        </Text>
+                        <View className='mt-6 mb-8'>
+                            <SearchInput initialQuery={query} />
                         </View>
                     </View>
                 )}
                 ListEmptyComponent={() => (
                     <EmptyState
                         title="No Videos Found"
-                        subtitle="Be the first one to upload a video ."
+                        subtitle="No videos found for this search query ."
                     />
                 )}
             />
--- components/SearchInput.jsx
+++ components/SearchInput.jsx
@@ -3,10 +3,10 @@ import React, { useState } from 'react'
 import { icons } from '../constants'
 import { router, usePathname } from 'expo-router'
 
-const SearchInput = () => {
+const SearchInput = ({ initialQuery }) => {
     const pathName = usePathname();
 
-    const [query, setQuery] = useState("");
+    const [query, setQuery] = useState(initialQuery || "");
 
     return (
         <View className='border-2 border-black-200 w-full h-16 px-4
