c35f8b7ead3733ac6cb29c50e973df93945920e0
Fitiavana Sambatra <Ghostrex2@gmail.com>
2024-05-29 23:49:02 +0300

    feat : video upload

--- app/(tabs)/create.jsx
+++ app/(tabs)/create.jsx
@@ -1,7 +1,8 @@
-import { Text, ScrollView } from 'react-native'
+import { Text, ScrollView, TouchableOpacity } from 'react-native'
 import React, { useState } from 'react'
 import { SafeAreaView } from 'react-native-safe-area-context'
 import FormField from '../../components/FormField'
+import { Video } from 'expo-av'
 
 const Create = () => {
     const [form, setForm] = useState({
@@ -29,6 +30,30 @@ const Create = () => {
                     handleChangeText={(e) => setForm({ ...form, title: e })}
                     otherStyles="mt-10"
                 />
+
+                <View className="mt-7 space-y-2">
+                    <Text className='text-base text-gray-100 font-p-medium'>
+                        Upload Video
+                    </Text>
+                    <TouchableOpacity>
+                        {
+                            form.video ?
+                                <Video>
+
+                                </Video>
+                                :
+                                (
+                                    <View className="w-full h-40 px-4 bg-black-100 rounded-2xl
+                                        justify-center items-center
+                                    ">
+                                        <View className="h-14 w-14 border border-dashed border-secondary-100 justify-center items-center">
+
+                                        </View>
+                                    </View>
+                                )
+                        }
+                    </TouchableOpacity>
+                </View>
             </ScrollView>
         </SafeAreaView>
     )
