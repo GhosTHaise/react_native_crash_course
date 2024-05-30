571a72203795a6f49a98dc0245ba1f2048dc592c
Fitiavana Sambatra <Ghostrex2@gmail.com>
2024-05-30 21:21:27 +0300

    feat : video upload file

--- app/(tabs)/create.jsx
+++ app/(tabs)/create.jsx
@@ -1,8 +1,9 @@
-import { Text, ScrollView, TouchableOpacity } from 'react-native'
+import { Text, ScrollView, TouchableOpacity, View, Image } from 'react-native'
 import React, { useState } from 'react'
 import { SafeAreaView } from 'react-native-safe-area-context'
 import FormField from '../../components/FormField'
-import { Video } from 'expo-av'
+import { ResizeMode, Video } from 'expo-av'
+import { icons } from '../../constants'
 
 const Create = () => {
     const [form, setForm] = useState({
@@ -38,16 +39,24 @@ const Create = () => {
                     <TouchableOpacity>
                         {
                             form.video ?
-                                <Video>
-
-                                </Video>
+                                <Video
+                                    source={{ uri: form.video.uri }}
+                                    className='w-full h-64 rounded-2xl'
+                                    useNativeControls
+                                    resizeMode={ResizeMode.COVER}
+                                    isLooping
+                                />
                                 :
                                 (
                                     <View className="w-full h-40 px-4 bg-black-100 rounded-2xl
                                         justify-center items-center
                                     ">
                                         <View className="h-14 w-14 border border-dashed border-secondary-100 justify-center items-center">
-
+                                            <Image
+                                                source={icons.upload}
+                                                resizeMode='contain'
+                                                className='w-1/2 h-1/2'
+                                            />
                                         </View>
                                     </View>
                                 )
