b5b74d0bbcd0dcfd21f7d045ac66fca4b7d05ae7
Fitiavana Sambatra <Ghostrex2@gmail.com>
2024-05-31 21:37:12 +0300

    feat : get file

--- app/(tabs)/create.jsx
+++ app/(tabs)/create.jsx
@@ -5,6 +5,7 @@ import FormField from '../../components/FormField'
 import { ResizeMode, Video } from 'expo-av'
 import { icons } from '../../constants'
 import CustomButton from '../../components/CustomButton'
+import * as DocumentPicker from "expo-document-picker"
 
 const Create = () => {
     const [uploading, setUploading] = useState(false);
@@ -16,7 +17,13 @@ const Create = () => {
     })
 
     const openPicker = async (selectType) => {
-
+        const result = await DocumentPicker.getDocumentAsync({
+            type: selectType === "image"
+                ?
+                ['image/png', 'image/jpg']
+                :
+                ['video/mp4', 'video/gif']
+        })
     }
 
     const handleSubmit = () => {
